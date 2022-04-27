2399ef1c7e4301bf3dda13ac7b7ae2687cd97ee9
clifford tsungu <ctsunguc@gmail.com>
2022-04-27 15:48:35 +0100

    ADDED FEATURES

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import { AuthContextProvider } from "../stores/authContext";
 import "../styles/globals.css";
@@ -6,7 +5,7 @@ import "../styles/navbar.css";
 import "../styles/header.css";
 import "../styles/howto.css";
 import "../styles/footer.css";
-import Howto from "../components/Howto";
+
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {

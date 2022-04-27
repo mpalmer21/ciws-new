567c56f0f5c1d93bf96ce94fcf65bef0cb9a8f25
clifford tsungu <ctsunguc@gmail.com>
2022-04-27 12:05:33 +0100

    new

--- pages/Steps_2/FinalStep.js
+++ pages/Steps_2/FinalStep.js
@@ -1,23 +1,23 @@
 import React from "react";
-import Email from "../Components/Email"
+import Email from "../Components/Email";
 import "./finalstep.css";
 const Finalstep = (props) => {
-  const { item,handleSubmit } = props;
+  const { item, handleSubmit } = props;
   const listItems = Object.entries(item).map(([key, value]) => (
     <div className="preview">
-    <li>
-      <strong>{key}:</strong> {value}
-    </li>
+      <li>
+        <strong key={value}>{key}:</strong> {value}
+      </li>
     </div>
   ));
   return (
     <div>
       <form onSubmit={handleSubmit}>
-      <ul>{listItems}</ul>
-      <button type="submit">Submit</button>
-      <Email />
+        <ul>{listItems}</ul>
+        <button type="submit">Submit</button>
+        <Email />
       </form>
     </div>
   );
 };
-export default Finalstep ;
+export default Finalstep;
